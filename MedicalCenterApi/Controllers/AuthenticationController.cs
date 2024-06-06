630b759ab710555f188bef1a919f2b55a80a577c
AZDELL\ali_zafar <ali.zaffar@gmail.com>
2024-06-06 19:44:11 -0400

    Clean Architecture

---
+++ MedicalCenterApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using Contracs.Authentication;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalCenterApi.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(RegisterRequest request)
+        {
+            return Ok(request);
+        }
+    }
+   
+}
