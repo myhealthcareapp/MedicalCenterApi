1c5877ee4548ec5918968753792841ceb0cb238b
AZDELL\ali_zafar <ali.zaffar@gmail.com>
2024-07-29 17:48:08 -0400

    Cleanup

--- ApplicationLayer/Services/Doctors/Dtos/Doctors/DoctorDto.cs
+++ ApplicationLayer/Services/Doctors/Dtos/Doctors/DoctorDto.cs
@@ -11,12 +11,5 @@ namespace Application.Services.Doctors.Dos.Doctors
     {
         public string Name { get; set; }
         public string Description { get; set; }
-
-        public static DoctorDto FromEntity(Doctor? doctor)
-        {
-            if (doctor == null) 
-                return null;
-            return new DoctorDto { Name = doctor.Name, Description = doctor.Description };
-        }
     }
 }
